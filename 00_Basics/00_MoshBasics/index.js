d97fe8260ffff6af4fa9940b16362a7fc714db26
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-01-22 04:59:03 +0530

    Data Types Primitive and reference

--- 00_Basics/00_MoshBasics/index.js
+++ 00_Basics/00_MoshBasics/index.js
@@ -3,8 +3,21 @@ let intrestRate = 0.3;
 intrestRate = 1 ;
 console.log(intrestRate);
 
-let name = 'Abhishek'; //string
+// const used for variables that dont change 
+
+// PRIMITIVE TYPES 
+let name = 'CODING WITH MOSH'; //string
 let age = 30;//number
+
+console.log(typeof(age)); //number
+age = 'hello';
+console.log(typeof(age)); //string
+
 let isApproved = false; //Boolean
+
 let firstName = undefined; // ~ default firstName = undefined ->let firstName;
+console.log(typeof(firstName)); //undefined
+
+// REFERENCE TYPES -> Object, ARRAY, FUNCTION 
 let selectedColor = null; // null => used to clear the value of variable
+console.log(typeof(selectedColor)); //object
