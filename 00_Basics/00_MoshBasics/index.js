1f71e225f32e14033d6d16f031792608a48e671d
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-01-22 04:47:28 +0530

    data types

--- 00_Basics/00_MoshBasics/index.js
+++ 00_Basics/00_MoshBasics/index.js
@@ -1,1 +1,10 @@
 console.log("Hello World");
+let intrestRate = 0.3;
+intrestRate = 1 ;
+console.log(intrestRate);
+
+let name = 'Abhishek'; //string
+let age = 30;//number
+let isApproved = false; //Boolean
+let firstName = undefined; // ~ default firstName = undefined ->let firstName;
+let selectedColor = null; // null => used to clear the value of variable
