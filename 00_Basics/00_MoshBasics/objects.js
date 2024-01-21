dd8d68caf3812407d4db86dc08dceb71483646c3
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-01-22 05:21:05 +0530

    Objects

--- 00_Basics/00_MoshBasics/objects.js
+++ 00_Basics/00_MoshBasics/objects.js
@@ -1,1 +1,13 @@
-console.log("Hello");
+let person = {
+    name:'Mosh',
+    age:30
+};
+
+console.log(person.name); //Mosh
+person.name = 'Abhishek';
+console.log(person.name);//Abhishek
+console.log(person['name']);//Abhishek
+
+// Dynamic input 
+let selection = 'age';
+console.log(person[selection]);//30
