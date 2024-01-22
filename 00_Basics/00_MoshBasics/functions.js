0c7cca7cffd5cdaab771339362d649178d309add
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-01-22 05:35:41 +0530

    functions

---
+++ 00_Basics/00_MoshBasics/functions.js
@@ -0,0 +1,15 @@
+// This function performs task
+function greet(name,lastName){
+    console.log("Hello "+name+' '+lastName);
+}
+
+greet('Mosh')
+//Hello Mosh undefined
+
+
+const square = (number)=>{
+    return number*number;
+}
+
+console.log(square(5));//25
+
