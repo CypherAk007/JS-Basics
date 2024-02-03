3b7f756e3ac6eb331ef371c14609a1c2075f87f5
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-02-03 17:57:52 +0530

    Toggle using querySelector

--- 02_JsErrorAsync&more/02_EventHandling/04_ToggleSwitch/index.js
+++ 02_JsErrorAsync&more/02_EventHandling/04_ToggleSwitch/index.js
@@ -1,16 +1,16 @@
-const tog = document.getElementsByClassName('toggle');
+const tog = document.querySelector('.toggle');
 let flag = true;
 const toggleEvent = ()=>{
     flag = !flag
     if (flag){
-        tog[0].classList.add('on')
+        tog.classList.add('on')
 
     }else{
 
-        tog[0].classList.remove('on')
+        tog.classList.remove('on')
     }
     
     console.log('clicked');
 }
 console.log(tog);
-tog[0].addEventListener('click',toggleEvent)
+tog.addEventListener('click',toggleEvent)
