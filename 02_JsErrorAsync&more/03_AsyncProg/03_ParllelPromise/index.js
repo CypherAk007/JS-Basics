cf228a9d9aff0534b97c2d9cf1924164cb4b86c8
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-02-04 20:42:03 +0530

    Parllel Promises

---
+++ 02_JsErrorAsync&more/03_AsyncProg/03_ParllelPromise/index.js
@@ -0,0 +1,30 @@
+const arr = ['https://example.com/data','https://example.com/data','https://example.com/data','https://example.com/data']
+
+// for(i=0;i<arr.length;i++){
+    
+// }
+const fetcher = async()=>{
+    try{
+        await out(arr)
+    }catch(err){
+        console.log(err);
+    }
+}
+
+const out =async(arr)=>{
+    arr.forEach((url,idx)=>{
+    setTimeout(()=>{
+        const data = fetch(url)
+        data.then((res)=>{
+            console.log(res);
+        })
+        .catch((err)=>{
+            console.log(idx+err.message);
+        })
+        .finally(()=>{
+            console.log(`${idx} has Ended!!`);
+        })
+    },idx*1000)
+})}
+
+fetcher()
