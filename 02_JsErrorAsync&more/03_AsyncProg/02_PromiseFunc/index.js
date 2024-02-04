98b8b6fd814200f045532f0824d12e484f6be114
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-02-04 20:16:53 +0530

    Fetch url

--- 02_JsErrorAsync&more/03_AsyncProg/02_PromiseFunc/index.js
+++ 02_JsErrorAsync&more/03_AsyncProg/02_PromiseFunc/index.js
@@ -1,18 +1,30 @@
-const event = new Promise((resolve,reject) =>{
-    var name = "k"
-    if (name==="Abhishek"){
-        resolve(name)
-    }else{
-        reject("Error name not matched")
-    }
-})
-
-event.then((name) =>{
-    console.log(name);
+// const axios  = require("axios")
+// const data = axios.get("url")
+const data = fetch('https://example.com/data')
+data.then((res)=>{
+    console.log(res);
 })
 .catch((err)=>{
-    console.log(err);
+    console.log(err.message);
 })
 .finally(()=>{
-    console.log('promise Finished');
+    console.log("promise Ended!!!");
 })
+// const event = new Promise((resolve,reject) =>{
+//     var name = "k"
+//     if (name==="Abhishek"){
+//         resolve(name)
+//     }else{
+//         reject("Error name not matched")
+//     }
+// })
+
+// event.then((name) =>{
+//     console.log(name);
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
+// .finally(()=>{
+//     console.log('promise Finished');
+// })
