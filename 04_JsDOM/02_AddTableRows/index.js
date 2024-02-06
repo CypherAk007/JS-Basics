152cf52e9afe975d80b56703cf421f2df100d887
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-02-06 16:37:13 +0530

    Edit TABLE row and col Not Working

--- 04_JsDOM/02_AddTableRows/index.js
+++ 04_JsDOM/02_AddTableRows/index.js
@@ -1,6 +1,8 @@
 const form = document.getElementById('form')
 form.addEventListener('submit',(event)=>{
     event.preventDefault()
+
+    // ********FOR ADDING ROWS******
     const inputs = form.querySelectorAll('input')
     const table = document.getElementById('tab')
     const tr = document.createElement('tr')
@@ -16,3 +18,16 @@ form.addEventListener('submit',(event)=>{
     })
     // console.log(inputs);
 })
+
+const onClickHandler = (event)=>{
+    //*********** / NOT WORKING *********
+    // **********FOR EDITING THE EXISTING ROWS***********
+    const rn = window.prompt("Input the Row number(0,1)", "0");
+    const cn = window.prompt("Input the Column number(0,1)","0");
+    const content = window.prompt("Input the Cell content");
+    console.log(rn,cn,content);
+    const table = document.getElementById('tab')
+    const row = table.rows[parseInt(rn)]
+    console.log(row[parseInt(cn)].value,row[parseInt(cn)].textContent,row[parseInt(cn)].innerHTML);
+    // row[parseInt(cn)].textContent = content
+}
